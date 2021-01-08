f8b684e0e2f4b6931394be34e4eb423b268c93ab
Mads Mogensen <mail@madsmogensen.dk>
2021-01-08 21:22:32 +0100

    java niceness

--- .config/bash/progs.sh
+++ .config/bash/progs.sh
@@ -2,3 +2,6 @@
 
 # Disable less history file
 export LESSHISTFILE=-
+
+# Make java programs look nicer
+_JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -DsDjdk.gtk.version=3"
