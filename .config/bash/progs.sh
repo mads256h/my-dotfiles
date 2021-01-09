c03e0fceed38c751c4c81cdeda70086f09e228e7
Mads Mogensen <mail@madsmogensen.dk>
2021-01-09 17:52:36 +0100

    Fix java

--- .config/bash/progs.sh
+++ .config/bash/progs.sh
@@ -4,4 +4,4 @@
 export LESSHISTFILE=-
 
 # Make java programs look nicer
-_JAVA_OPTIONS="-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -DsDjdk.gtk.version=3"
+export _JAVA_OPTIONS="-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -DsDjdk.gtk.version=3"
