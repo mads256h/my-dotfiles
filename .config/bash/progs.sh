3c73e62e8855683825519563ab11f71f72f2bbc2
Mads Mogensen <mail@madsmogensen.dk>
2021-01-15 12:36:24 +0100

    qt theme

--- .config/bash/progs.sh
+++ .config/bash/progs.sh
@@ -5,3 +5,6 @@ export LESSHISTFILE=-
 
 # Make java programs look nicer
 export _JAVA_OPTIONS="-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -DsDjdk.gtk.version=3"
+
+# Make qt use theme configured by qt5ct
+export QT_QPA_PLATFORMTHEME="qt5ct"
