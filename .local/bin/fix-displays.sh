f3a4e42b95727c7e1938333081a261d1ac0760ba
Mads Mogensen <mail@madsmogensen.dk>
2022-04-09 11:21:53 +0200

    add fix-display.sh

---
+++ .local/bin/fix-displays.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-2 --primary --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal --output DP-5 --mode 1920x1080 --pos 0x0 --rotate normal
