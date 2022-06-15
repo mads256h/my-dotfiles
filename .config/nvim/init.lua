f7fbd2f79e5eca83d46beb0151176741bce3e2d3
Mads Mogensen <mail@madsmogensen.dk>
2022-06-15 16:43:04 +0200

    nvim: add latex keybinds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -258,3 +258,84 @@ vim.api.nvim_set_keymap("n", "<space>n", '/<++><CR>ca<', {})
 
 -- Go to next <++?> and prompt to replace them all with input
 vim.api.nvim_set_keymap("n", "<space>b", '/<++.><CR>va<y/<C-r>"<CR>:%s/<C-r>"//g<left><left>', {})
+
+
+-- Latex code snippets
+local function latex_binding(suffix, vimCommand, mode)
+  vim.api.nvim_set_keymap(mode or "n", "<space>l" .. suffix, vimCommand, {})
+end
+
+local function latex_command(suffix, command, insertOp)
+  latex_binding(suffix, (insertOp or 'o') .. '\\' .. command .. '{}<left>')
+end
+
+local function latex_wrap(suffix, command)
+  latex_binding(suffix, 'c' .. '\\' .. command .. '{<c-r>"}<esc>', "v")
+end
+
+local function latex_command_and_wrap(suffix, command)
+  latex_command(suffix, command, "a")
+  latex_wrap(suffix, command)
+end
+
+local function latex_environment(suffix, env)
+  latex_binding(suffix, 'o\\begin{' .. env .. '}<CR><CR>\\end{' .. env .. '}<CR><up><up><tab>')
+end
+
+-- Compile with latexmk
+latex_binding("m", ':!latexmk<CR>')
+
+-- Text modifiers
+latex_command_and_wrap("tb", "textbf", "a")
+latex_command_and_wrap("ti", "textit", "a")
+latex_command_and_wrap("tt", "texttt", "a")
+latex_command_and_wrap("ts", "say", "a")
+
+-- Sections
+latex_command("ch", "chapter")
+latex_command("se", "section")
+latex_command("ss", "subsection")
+latex_command("sS", "subsubsection")
+latex_command("sp", "paragraph")
+latex_command("sP", "subparagraph")
+
+-- Lists
+latex_environment("li", "itemize")
+latex_environment("le", "enumerate")
+latex_environment("ld", "description")
+
+-- Math
+latex_environment("eq", "equation")
+latex_binding("es", 'o\\begin{equation}<CR>\\begin{aligned}<CR><CR>\\end{aligned}<CR>\\end{equation}<CR><ESC>4kV3j<ji<tab>')
+latex_environment("et", "theorem")
+latex_environment("ep", "proof")
+
+-- Figure
+latex_binding("fg", 'o\\begin{figure}[H]<CR>\\centering<CR>\\includegraphics[width=\\textwidth]{<++>}<CR>\\caption{<++>}<CR>\\label{fig:<++>}<CR>\\end{figure}<CR><ESC>4k0')
+latex_binding("fn", 'o\\begin{figure}[H]<CR>\\centering<CR><++><CR>\\caption{<++>}<CR>\\label{fig:<++>}<CR>\\end{figure}<CR><ESC>4k0')
+
+
+
+--[[
+" Sections
+nmap <space>lch o\chapter{<++>}<CR><ESC>k0
+nmap <space>lse o\section{<++>}<CR><ESC>k0
+nmap <space>lss o\subsection{<++>}<CR><ESC>k0
+nmap <space>lsS o\subsubsection{<++>}<CR><ESC>k0
+nmap <space>lsp o\paragraph{<++>}<CR><ESC>k0
+nmap <space>lsP o\subparagraph{<++>}<CR><ESC>k0
+
+" Math
+nmap <space>leq o\begin{equation}<CR><++><CR>\end{equation}<CR><ESC>2k0
+nmap <space>les o\begin{equation}<CR>\begin{aligned}<CR><++><CR>\end{aligned}<CR>\end{equation}<CR><ESC>3k0
+nmap <space>let o\begin{theorem}<CR><++><CR>\end{theorem}<CR><ESC>2k0
+nmap <space>lep o\begin{proof}<CR><++><CR>\end{proof}<CR><ESC>2k0
+
+" Figure
+nmap <space>lfg o\begin{figure}[H]<CR>\centering<CR>\includegraphics[width=\textwidth]{<++>}<CR>\caption{<++>}<CR>\label{fig:<++>}<CR>\end{figure}<CR><ESC>4k0
+nmap <space>lfn o\begin{figure}[H]<CR>\centering<CR><++><CR>\caption{<++>}<CR>\label{fig:<++>}<CR>\end{figure}<CR><ESC>4k0
+
+" Listings
+nmap <space>llc o\begin{listing}[H]<CR>\begin{minted}[breaklines, linenos]{<++>}<CR><++><CR>\end{minted}<CR>\caption{<++>}<CR>\label{lst:<++>}<CR>\end{listing}<CR><ESC>6k0Vj:s/^  //<CR>:noh<CR>k
+nmap <space>lli o\begin{listing}[H]<CR>\inputminted[breaklines, linenos, firstline=<++>, lastline=<++>]{<++>}{<++>}<CR>\caption{<++>}<CR>\label{lst:<++>}<CR>\end{listing}<CR><ESC>4k0
+--]]
