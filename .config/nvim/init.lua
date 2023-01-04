c87ed1e18e3eaeb51dbcec0136855fffb1e8e265
Mads Mogensen <mail@madsmogensen.dk>
2023-01-04 21:00:55 +0100

    update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -128,7 +128,7 @@ end
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'texlab', 'bashls', 'cmake', 'cssls', 'eslint', 'html', 'jsonls' }
