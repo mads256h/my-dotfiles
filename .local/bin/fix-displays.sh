df38a5b821e6ccba22e13900c0d2d358b5fe0b5a
Mads Mogensen <mail@madsmogensen.dk>
2023-09-20 15:28:58 +0200

    Update fix-displays.sh to reflect my new setup

--- .local/bin/fix-displays.sh
+++ .local/bin/fix-displays.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 xrandr \
-  --output DP-5    --mode 1920x1080 --rate 60 --pos 0x0 --rotate normal\
-  --output DVI-I-1 --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal\
-  --output DP-2    --mode 1920x1080 --rate 240  --pos 3840x0 --rotate normal --primary\
-  --output HDMI-0  --mode 1920x1080 --rate 60  --pos 5760x0 --rotate normal\
+  --output DP-4    --mode 1920x1080 --rate 240  --pos 0x0 --rotate normal --primary\
+  --output DVI-I-1 --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal
