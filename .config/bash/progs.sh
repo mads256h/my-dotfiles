f048fa113440d6a871e6ddca0c52a9cb3e3d61ae
Mads Mogensen <mail@madsmogensen.dk>
2023-12-20 18:11:13 +0100

    update

--- .config/bash/progs.sh
+++ .config/bash/progs.sh
@@ -4,7 +4,7 @@
 export LESSHISTFILE=-
 
 # Make java programs look nicer
-export _JAVA_OPTIONS="-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -DsDjdk.gtk.version=3 -Djdk.downloader.home=$XDG_DATA_HOME/jdks"
+export _JAVA_OPTIONS="-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Djdk.gtk.version=3 -Djdk.downloader.home=$XDG_DATA_HOME/jdks"
 
 # Make qt use theme configured by qt5ct
 export QT_QPA_PLATFORMTHEME="qt5ct"
@@ -17,3 +17,6 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
 # dotnet root
 export DOTNET_ROOT="/opt/dotnet"
+
+# Use nvidia libva driver
+[ "$HOSTNAME" = "DESKTOP-MADS" ] && export LIBVA_DRIVER_NAME=nvidia
