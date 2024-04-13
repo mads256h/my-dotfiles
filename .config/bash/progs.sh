3be8ee164bf258f388808b0606e05ec25d7941b4
Mads Mogensen <mail@madsmogensen.dk>
2024-04-13 22:03:20 +0200

    java: use font's aa settings

--- .config/bash/progs.sh
+++ .config/bash/progs.sh
@@ -5,7 +5,7 @@ export LESSHISTFILE=-
 
 # Make java programs look nicer
 # and move stuff out of the home directory
-export _JAVA_OPTIONS="-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Djdk.gtk.version=3 -Djdk.downloader.home=$XDG_DATA_HOME/jdks -Djavafx.cachedir=$XDG_CACHE_HOME/openjfx"
+export _JAVA_OPTIONS="-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java -Dawt.useSystemAAFontSettings=gasp -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Djdk.gtk.version=3 -Djdk.downloader.home=$XDG_DATA_HOME/jdks -Djavafx.cachedir=$XDG_CACHE_HOME/openjfx"
 
 # Make qt use theme configured by qt5ct
 export QT_QPA_PLATFORMTHEME="qt5ct"
