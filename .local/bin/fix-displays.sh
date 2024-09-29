5498c72b8a940543b06c85314efcc3732809697a
Mads Mogensen <mail@madsmogensen.dk>
2024-09-29 11:22:58 +0200

    update

--- .local/bin/fix-displays.sh
+++ .local/bin/fix-displays.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr \
   --output DP-4    --mode 1920x1080 --rate 240  --pos 0x0 --rotate normal --primary\
-  --output DVI-I-1 --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal
+  --output HDMI-0 --mode 1920x1080 --rate 60 --pos 1920x0 --rotate normal
