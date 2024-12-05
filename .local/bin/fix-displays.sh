cf27630c77a3e5b162944f42542b0e3a733c976f
Mads Mogensen <mail@madsmogensen.dk>
2024-12-05 21:41:53 +0100

    fix fix-displays

--- .local/bin/fix-displays.sh
+++ .local/bin/fix-displays.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr \
-  --output DP-4    --mode 1920x1080 --rate 240  --pos 0x0 --rotate normal --primary\
+  --output DP-0    --mode 1920x1080 --rate 240  --pos 0x0 --rotate normal --primary\
   --output HDMI-0 --mode 1920x1080 --rate 60 --pos 1920x0 --rotate normal
